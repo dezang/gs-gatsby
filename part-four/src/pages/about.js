712b4ca6d75ba00a64a0a9d2179d1061e7279cc2
Dezang <dezang@Dezangui-iMac-Pro.local>
2019-07-24 01:16:08 +0900

    Done initial pages setting

---
+++ part-four/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>About nesciunt alias velit</h1>
+    <p>
+      Ad blanditiis qui libero ut. Voluptas eligendi mollitia. Reprehenderit
+      deserunt natus consectetur.
+    </p>
+  </Layout>
+)
--- part-four/src/pages/index.js
+++ part-four/src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <h1>Ducimus nesciunt eius non ut recusandae.</h1>
+    <div>
+      <img src="http://lorempixel.com/640/480/animals" alt="animals" />
+    </div>
+  </Layout>
+)
